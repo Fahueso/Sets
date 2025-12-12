3ff8d7ac816e947fb803bb29a19dc65f09a6310e
fahueso <fa.huesopastor@edu.gva.es>
2025-12-12 13:29:09 +0100

    Primera versión BDEdades

---
+++ src/BDEdades.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class BDEdades {
+    public static void main(String[] args) {
+        HashMap<String,Integer> nombreEdad = new HashMap<>();
+
+        nombreEdad.put("Alfonso",15);
+        nombreEdad.put("Manuel", 20);
+        nombreEdad.put("Andrea", 25);
+
+
+        int acu=0; int menor=Integer.MAX_VALUE;
+        String nombreMenor="";
+        for(Map.Entry<String, Integer> pareja: nombreEdad.entrySet()){
+            System.out.printf(pareja.getKey());
+            acu+=pareja.getValue();
+            if(pareja.getValue()<menor){
+                menor = pareja.getValue();
+                nombreMenor= pareja.getKey();
+            }
+        }
+        System.out.println("La media es " + acu/nombreEdad.size());
+        System.out.println("El más joven es " + nombreMenor + " con " + menor);
+    }
+}
